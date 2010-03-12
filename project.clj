e5fb2e1776a0110bc34ef66d6fbe908a877e1446
Mark McGranaghan <mmcgrana@gmail.com>
2010-03-11 20:06:26 -0500

    Depend on stable libs.

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :javac-fork "true"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[clj-json "0.1.0-SNAPSHOT"]
-                     [lein-clojars "0.5.0-SNAPSHOT"]
-                     [lein-javac "0.0.2-SNAPSHOT"]])
+  :dev-dependencies [[clj-json "0.2.0"]
+                     [lein-clojars "0.5.0"]
+                     [org.clojars.mmcgrana/lein-javac "0.1.0"]])
